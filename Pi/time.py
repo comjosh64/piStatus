ee9509b7a27983263af1f030bd65a177d341bb8f
comjosh64 <comjosh64@gmail.com>
2015-03-01 19:39:25 -0600

    Update time.py
    SecUp

--- Pi/time.py
+++ Pi/time.py
@@ -1,6 +1,8 @@
 import requests
 import socket
 import datetime
+import credentials
+
 
 timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
 timestamp = timestamp + "\n"
@@ -13,6 +15,6 @@ hostname = socket.gethostname()
 intro = hostname + "@"
 status = "\nStatus:Online\n"
 update = timestamp + intro + my_IP + status
-post_data = {'access_token':'d5f512ede31ddf74510d043a6bb9afe57952cad8', 'args':update}
-post_response = requests.post(url='https://api.spark.io/v1/devices/54ff72066678574947220367/prntscr', data=post_data)
+post_data = {'access_token':credentials.access_token, 'args':update}
+post_response = requests.post(url=credentials.deviceURL, data=post_data)
 print post_response
