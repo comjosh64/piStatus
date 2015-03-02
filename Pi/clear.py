d1ad186da6712cd636de1761706fd2b99af2f655
comjosh64 <comjosh64@gmail.com>
2015-03-01 19:37:53 -0600

    Update clear.py
    added import

--- Pi/clear.py
+++ Pi/clear.py
@@ -1,4 +1,5 @@
 import requests
+import credentials
 
 post_data = {'access_token':credentials.access_token, 'args':'CLEAR'}
 post_response = requests.post(url=credentials.deviceURL, data=post_data)
