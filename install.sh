61c7e7ef723c0f813ed0a1fdb065564dd47f3bf2
Josh Krull <comjosh64@gmail.com>
2015-03-02 05:44:33 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+
+sudo apt-get -y install git pkg-config libevent-pthreads-2.0-5 libao-dev libgnutls28-dev libmad0-dev libfaad-dev libjson0-dev libgcrypt11-dev python-requests python-dev
+
+
+cd
+git clone https://github.com/giampaolo/psutil.git
+cd psutil
+sudo python setup.py install
+
+
+
+
+
+
+
+cd
+git clone https://github.com/comjosh64/piStatus.git
