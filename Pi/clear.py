3233ae72b1319b2bec97630d18560957d6e1362a
comjosh64 <comjosh64@gmail.com>
2015-03-01 19:35:24 -0600

    Update clear.py
    Security Update

--- Pi/clear.py
+++ Pi/clear.py
@@ -1,5 +1,5 @@
 import requests
 
-post_data = {'access_token':'d5f512ede31ddf74510d043a6bb9afe57952cad8', 'args':'CLEAR'}
-post_response = requests.post(url='https://api.spark.io/v1/devices/54ff72066678574947220367/prntscr', data=post_data)
+post_data = {'access_token':credentials.access_token, 'args':'CLEAR'}
+post_response = requests.post(url=credentials.deviceURL, data=post_data)
 print post_response
